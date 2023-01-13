a539525842fcf96fc95014447fdbe8c792be5b05
Luc2023 <122548007+Luc2023@users.noreply.github.com>
2023-01-13 11:20:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,13 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Asleep)
 })
-basic.showIcon(IconNames.Meh)
+music.playSoundEffect(music.createSoundEffect(
+WaveShape.Sine,
+5000,
+0,
+255,
+0,
+500,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+), SoundExpressionPlayMode.InBackground)
