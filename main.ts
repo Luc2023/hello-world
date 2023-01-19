39605853e1e0a8e779b034c7533cb184b3cf0df9
Luc2023 <122548007+Luc2023@users.noreply.github.com>
2023-01-19 11:21:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
 input.onButtonPressed(Button.A, function () {
-    music.playMelody("C5 E A D G B E C5 ", 1250)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . # # .
+        . . . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(randint(0, 10))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Happy)
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    rps = randint(1, 3)
+    if (rps == 1) {
+        basic.showString("R")
+    } else if (rps == 2) {
+        basic.showString("P")
+    } else {
+        basic.showString("S")
+    }
 })
-music.playMelody("B F B A B F C F ", 5000)
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    music.playMelody("A F D F B E C B ", 1200)
+})
+let rps = 0
+basic.showIcon(IconNames.Asleep)
