b86c2f83bd37db0f075f7023924ba38ad129750f
Luc2023 <122548007+Luc2023@users.noreply.github.com>
2023-01-20 11:17:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
+let letter = 0
+let rps = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # # # .
-        # . . . #
-        . . # # .
-        . . . . .
-        . . # . .
-        `)
+    for (let index = 0; index < 10; index++) {
+        basic.showString("A",5)
+basic.showString("B",5)
+basic.showString("C",5)
+basic.showString("D",5)
+    }
+    letter = randint(1, 4)
+    if (letter == 1) {
+        basic.showString("A")
+    } else if (letter == 2) {
+        basic.showString("B")
+    } else if (letter == 3) {
+        basic.showString("C")
+    } else {
+        basic.showString("D")
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(randint(0, 10))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
-})
-input.onGesture(Gesture.Shake, function () {
     rps = randint(1, 3)
     if (rps == 1) {
         basic.showString("R")
@@ -23,8 +31,47 @@ input.onGesture(Gesture.Shake, function () {
         basic.showString("S")
     }
 })
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    music.playMelody("A F D F B E C B ", 1200)
+input.onGesture(Gesture.Shake, function () {
+	
 })
-let rps = 0
-basic.showIcon(IconNames.Asleep)
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            . . # # .
+            . . . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `,50)
+basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . . # #
+        # # # # #
+        . # # # .
+        `,50)
+basic.showLeds(`
+        . # # # .
+        . . . # #
+        . . . # #
+        . . . # #
+        . # # # .
+        `,50)
+basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . . # #
+        # # # # #
+        . # # # .
+        `,50)
+})
